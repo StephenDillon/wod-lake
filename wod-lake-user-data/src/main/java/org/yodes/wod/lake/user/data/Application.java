522bb25cfbe413f3989e96ab4f4b8ba322eaf3b8
Stephen Dillon <stephen.dillon@emc.com>
2016-03-22 16:56:48 -0400

    Updating spring versions and adding dev tools

--- wod-lake-user-data/src/main/java/org/yodes/wod/lake/user/data/Application.java
+++ wod-lake-user-data/src/main/java/org/yodes/wod/lake/user/data/Application.java
@@ -1,7 +1,6 @@
 package org.yodes.wod.lake.user.data;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
